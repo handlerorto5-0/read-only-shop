f07968d7e2546bd60bf1f67cc80cc987cc409b03
kv-2 <str_token@mail.ru>
2019-06-20 19:43:08 +0300

    update controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,4 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:name, :price)
-    end
 end
